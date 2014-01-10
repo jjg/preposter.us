97260ee19932502feb78e3df9f9e15d5c96faac5
Jason J. Gullickson <jason.gullickson@gmail.com>
2014-01-10 09:32:28 -0600

    bug

--- preposterous.py
+++ preposterous.py
@@ -271,7 +271,7 @@ if uid_list[0] != '':
 			new_post = new_post.replace('{0}', post_title)
 			new_post = new_post.replace('{1}', post_author)
 			new_post = new_post.replace('{2}', post_body)
-			new_post = new_post.replace('{3}', dateutil.parser(post_date).strftime('%b %d, %Y')
+			new_post = new_post.replace('{3}', dateutil.parser(post_date).strftime('%b %d, %Y'))
 			
 			post_file = open(post_physical_path, 'w')
 			post_file.write(new_post)
